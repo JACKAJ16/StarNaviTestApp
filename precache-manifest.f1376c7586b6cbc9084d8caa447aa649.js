f56c125efa16ce0172fb50cbd1b4c7845b56f030
JACK <zhenya271@gmail.com>
2020-03-26 18:36:54 +0200

    Updates

---
+++ precache-manifest.f1376c7586b6cbc9084d8caa447aa649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0389117cdc49608a622d6d560325d56",
+    "url": "/StarNaviTestApp/index.html"
+  },
+  {
+    "revision": "1a2869ddce60f8126156",
+    "url": "/StarNaviTestApp/static/css/main.b9855145.chunk.css"
+  },
+  {
+    "revision": "c312c9e64887f1ac02ba",
+    "url": "/StarNaviTestApp/static/js/2.83de6c89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StarNaviTestApp/static/js/2.83de6c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2869ddce60f8126156",
+    "url": "/StarNaviTestApp/static/js/main.47b55169.chunk.js"
+  },
+  {
+    "revision": "f247a68e85570054e286",
+    "url": "/StarNaviTestApp/static/js/runtime-main.d1e807d1.js"
+  }
+]);
